817d33db1dae78c306fb7e2b141e29d6bdcbd403
Sourav Barua <sourav.shr6@hotmail.com>
2019-10-18 17:17:00 -0230

    Fixed the polyfills.ts issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,30 +8,32 @@ import * as AWS from 'aws-sdk';
 })
 export class AppComponent {
   title = 'Natures-Palette';
-  image: any;
+  uploadedFile: any;
   fileEvent(fileInput: any) {
-    console.log('test');
-    //const AWSService = AWS;
-    // const region = 'Global';
-    // const bucketName = 'naturespalette';
-    // const IdentityPoolId = 'npidentitypool';
-    // const file = fileInput.target.files[0]; // Configures the AWS service and initial authorization
-  //   AWSService.config.update({
-  //     region: region,
-  //     credentials: new AWSService.CognitoIdentityCredentials({
-  //       IdentityPoolId: IdentityPoolId
-  //     })
-  //   });
-  //   // adds the S3 service, make sure the api version and bucket are correct
-  //   const s3 = new AWSService.S3({
-  //     apiVersion: '2006-03-01',
-  //     params: { Bucket: bucketName}
-  //   });
-  //   // I store this in a variable for retrieval later
-  //   this.image = file.name;  s3.upload({ Key: file.name, Bucket: bucketName, Body: file, ACL: 'public-read'}, function(err, data) {
-  //    if (err) {
-  //      console.log(err, 'there was an error uploading your file');
-  //    }
-  //  });
+
+    const AWSService = AWS;
+    const region = 'Canada(Central)';
+    const bucketName = 'naturespalette';
+    const IdentityPoolId = 'npidentitypool';
+    const file = fileInput.target.files[0]; // Configures the AWS service and initial authorization
+    console.log(file);
+    AWSService.config.update({
+      region: region,
+      credentials: new AWSService.CognitoIdentityCredentials({
+        IdentityPoolId: IdentityPoolId
+      })
+    });
+    // adds the S3 service, make sure the api version and bucket are correct
+    const s3 = new AWSService.S3({
+      apiVersion: '2006-03-01',
+      params: { Bucket: bucketName}
+    });
+    // I store this in a variable for retrieval later
+    this.uploadedFile = file.name;
+    s3.upload({ Key: file.name, Bucket: bucketName, Body: file, ACL: 'public-read'}, function(err, data) {
+     if (err) {
+       console.log(err, 'there was an error uploading your file');
+     }
+   });
   }
 }
