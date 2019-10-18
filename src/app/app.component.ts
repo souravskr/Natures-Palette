18829e84d08f628a37824da3bae1c7d5cbe9b050
Sourav Barua <sourav.shr6@hotmail.com>
2019-10-18 18:39:45 -0230

    Wiped up s3 integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,31 +9,5 @@ import * as AWS from 'aws-sdk';
 export class AppComponent {
   title = 'Natures-Palette';
   uploadedFile: any;
-  fileEvent(fileInput: any) {
 
-    const AWSService = AWS;
-    const region = 'Canada(Central)';
-    const bucketName = 'naturespalette';
-    const IdentityPoolId = 'npidentitypool';
-    const file = fileInput.target.files[0]; // Configures the AWS service and initial authorization
-    console.log(file);
-    AWSService.config.update({
-      region: region,
-      credentials: new AWSService.CognitoIdentityCredentials({
-        IdentityPoolId: IdentityPoolId
-      })
-    });
-    // adds the S3 service, make sure the api version and bucket are correct
-    const s3 = new AWSService.S3({
-      apiVersion: '2006-03-01',
-      params: { Bucket: bucketName}
-    });
-    // I store this in a variable for retrieval later
-    this.uploadedFile = file.name;
-    s3.upload({ Key: file.name, Bucket: bucketName, Body: file, ACL: 'public-read'}, function(err, data) {
-     if (err) {
-       console.log(err, 'there was an error uploading your file');
-     }
-   });
-  }
 }
