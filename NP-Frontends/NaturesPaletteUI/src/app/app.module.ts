7bb023ffd2c501b4c0797ba1db04fc9f62a0d09e
Sourav Barua <sourav.shr6@hotmail.com>
2019-11-03 14:45:24 -0330

    Changed The previous file uploading server's alias to NP-Express-Server.

--- NP-Frontends/NaturesPaletteUI/src/app/app.module.ts
+++ NP-Frontends/NaturesPaletteUI/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomecomponentComponent } from './PageComponents/homecomponent/homecomponent.component';
 
+// TODO: Vivek [You need to add any modules that I ask you to do into this is the base module for the entire application]
 @NgModule({
   declarations: [
     AppComponent,
